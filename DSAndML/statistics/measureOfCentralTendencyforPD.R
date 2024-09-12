c0899abd3127eb34a94e3ccbc81097550d95bc52
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-09-12 13:56:50 +0530

    Variance implemented in R

--- DSAndML/statistics/measureOfCentralTendencyforPD.R
+++ DSAndML/statistics/measureOfCentralTendencyforPD.R
@@ -8,6 +8,19 @@ expectedValue <- function(distribution) {
     return(Mean)
 }
 
+
+# Function to calculate variance value
+variance <- function(distribution) {
+    Mean <- expectedValue(distribution)
+    Var <- 0
+    # Loop through each element of the distribution
+    for (i in seq_along(distribution)) {
+        X_squared <- (i) ** 2
+        Var <- Var  +  (X_squared * distribution[i])
+    }
+    return(Var - (Mean ** 2))
+}
+
 expectedValue_1 <- function(distribution) {
     Mean <- 0
     for (i in 1:length(distribution)) {
@@ -23,3 +36,6 @@ distribution <- c(1/36, 3/36, 5/36, 7/36, 9/36, 11/36)
 expected_val <- expectedValue(distribution)
 print(paste("Expected Value:", round(expected_val,2)))
 
+# Calculate and print the variance value
+variance_val <- variance(distribution)
+print(paste("Variance Value:", round(variance_val,2)))
